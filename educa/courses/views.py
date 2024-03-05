046d4634654204b3d4457fa5bdf3ec87ef4bb272
Alexander <alexander@iMac-Alexander.local>
2024-03-05 09:59:34 +0300

    manage course complete

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,44 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+from .models import Course
+
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
+    
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+     template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    # template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.add_course'
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    # template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.change_course'
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
@@ -24,6 +24,7 @@ urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name='login'),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name='logout'),
     path("admin/", admin.site.urls),
+    path("course/", include('courses.urls')),
 ]
 
 if settings.DEBUG:
