f89d12f0d41381e3672297b2a714b3a6d071bb22
Alexander <alexander@iMac-Alexander.local>
2024-03-07 09:13:02 +0300

    list courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.apps import apps
+from django.db.models import Count
 from django.db.models.query import QuerySet
 from django.forms import modelform_factory
 from django.http import HttpRequest
@@ -7,14 +8,14 @@ from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponseMixin, View
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from braces.views  import CsrfExemptMixin, JsonRequestResponseMixin
 
 from .forms import ModuleFormSet
 
 
-from .models import Content, Course, Module
+from .models import Content, Course, Module, Subject
 
 
 
@@ -157,3 +158,24 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+    
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses,
+        })
+    
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
