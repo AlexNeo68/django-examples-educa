42783dc25d235e4c9d3550cee38d1560090d6363
Alexander <alexander@iMac-Alexander.local>
2024-03-09 19:45:17 +0300

    before drf

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import StudentCourseDetailView, StudentCourseListView, StudentEnrollCourseView, StudentRegistrationView
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
     path('register/', StudentRegistrationView.as_view(), name='student_registration'),
     path('enroll-course/', StudentEnrollCourseView.as_view(), name='student_enroll_course'),
     path('courses/', StudentCourseListView.as_view(), name='student_course_list'),
-    path('courses/<pk>/', StudentCourseDetailView.as_view(), name='student_course_detail'),
-    path('courses/<pk>/<module_id>/', StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+    path('courses/<pk>/', cache_page(60*15)(StudentCourseDetailView.as_view()), name='student_course_detail'),
+    path('courses/<pk>/<module_id>/', cache_page(60*15)(StudentCourseDetailView.as_view()), name='student_course_detail_module'),
 ]
