c069bb4b216ff12836fa33745fb0d4a78f450aa7
Alexander <alexander@iMac-Alexander.local>
2024-03-10 14:03:05 +0300

    action in view set

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,10 +1,11 @@
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 
 
@@ -18,11 +19,13 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication],
+            permisssion_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
