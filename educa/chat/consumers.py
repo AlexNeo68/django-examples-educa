9a355d805212a84cd889db367bb9291c5ad64090
Alexander <alexander@iMac-Alexander.local>
2024-03-12 10:00:39 +0300

    before go to deployment

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,37 +1,38 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.utils import timezone
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.user = None
         self.room_group_name = None
         self.id = None
 
-    def connect(self):
+    async def connect(self):
         self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
-        async_to_sync(self.channel_layer.group_add)(
+
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name,
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         now = timezone.now()
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -41,5 +42,5 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def chat_message(self, event):
-        self.send(text_data=json.dumps(event))
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
